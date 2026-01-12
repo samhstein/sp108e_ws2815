df5e9acd2c20edad513b804adada6d8ee5d9b66f
sam stein <sam@mega.net>
2026-01-12 16:54:51 -0700

    Update options_flow.py

--- options_flow.py
+++ options_flow.py
@@ -7,13 +7,13 @@ ALL_EFFECTS = list({**MONO_EFFECTS, **PRESET_EFFECTS}.keys())
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
+        self._config_entry = config_entry  # <-- use a private attr
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        current = {**self.config_entry.data, **self.config_entry.options}
+        current = {**self._config_entry.data, **self._config_entry.options}
 
         return self.async_show_form(
             step_id="init",
